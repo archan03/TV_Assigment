72feb4e349d095595f0e91177f4f9e63f12f9437
archan03 <bhalaniarchan@gmail.com>
2023-02-10 17:56:59 +0530

    Remove

--- CI Platform/CI Platform/CI Platform/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CI_Platform.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
